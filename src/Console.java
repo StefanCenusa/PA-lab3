5c5c3e7bb553c8635ec960e4e9c07438431c323c
Stefan Cenușă <cenusa.s@gmail.com>
2015-03-14 12:24:20 +0200

    done

--- src/Console.java
+++ src/Console.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class Console {
@@ -26,21 +28,28 @@ public class Console {
     }
 
     public void list(String arguments) {
-        if (arguments == null) arguments = ".";
+        boolean found = false;
+        if (arguments.equals(".")) arguments = System.getProperty("user.dir");
+
         File folder = new File(arguments);
         File[] fileList = folder.listFiles();
         if (fileList == null)
             throw new IllegalArgumentException("Folder gresit!");
+
         for (File fileEntry : fileList) {
             if (!fileEntry.isDirectory()) {
                 String name = fileEntry.getName();
                 if (name.contains(".")) {
                     String ext = name.substring(name.lastIndexOf('.')+1);
                     if (Arrays.asList(fileTypes).contains(ext.toLowerCase())) {
+                        found = true;
                         System.out.println(name);
                     }
                 }
             }
+        }
+        if (!found){
+            System.out.println("Niciun fisier audio!");
         }
     }
 
@@ -51,5 +60,59 @@ public class Console {
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public void cd(String directory_name)
+    {
+        File directory;
+
+        directory = new File(directory_name).getAbsoluteFile();
+        if (directory.exists() || directory.mkdirs()) {
+            System.setProperty("user.dir", directory.getAbsolutePath());
+        }
+        pwd();
+    }
+
+    public void search (Pattern p, String pDir){
+        File dir = new File (pDir);
+
+        File[] files = dir.listFiles();
+        if (files == null) return;
+        for (int i = 0; i < files.length; i++) {
+            String x = files[i].getName();
+            if (".".equals(x)) continue;
+            if ("..".equals(x)) continue;
+
+            Matcher m = p.matcher(x);
+            if (m.matches()) {
+                System.out.println(x);
+            }
+            if (files[i].isDirectory ()) {
+                search(p, pDir + File.separator + x);
+            }
+        }
+    }
+
+    public void rename(String oldname, String newname){
+        // File (or directory) with old name
+        File file = new File(oldname);
+
+        // File (or directory) with new name
+        File file2 = new File(newname);
+        if(file2.exists()) try {
+            throw new IOException("file exists");
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+
+        // Rename file (or directory)
+        boolean success = file.renameTo(file2);
+        if (!success) {
+            throw new IllegalArgumentException("Eroare la redenumire!");
+        }
+    }
+
+    public void pwd(){
+        System.out.println("Working Directory: "+ System.getProperty("user.dir"));
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
@@ -6,29 +7,53 @@ public class Main {
         while (true) {
             String[] commandParts = con.readCommand().split(" ");
             try {
-                if(commandParts[0].equals("list")){
-                    if (commandParts.length < 2) {
-                        con.list(null);
+                if (commandParts[0].equals("list")) {
+                    if (commandParts.length == 1) {
+                        con.list(System.getProperty("user.dir"));
                         continue;
-                    }
-                    else
-                        if (commandParts.length==2) {
-                            con.list(commandParts[1]);
-                            continue;
-                        }
-                        else throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
+                    } else if (commandParts.length == 2) {
+                        con.list(commandParts[1]);
+                        continue;
+                    } else throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
                 }
-                if(commandParts[0].equals("play")){
-                    if (commandParts.length==2) {
+                if (commandParts[0].equals("play")) {
+                    if (commandParts.length == 2) {
                         con.play(commandParts[1]);
                         continue;
-                    }
+                    } else if (commandParts.length == 1)
+                        throw new IllegalArgumentException("Nu ati specificat fisierul!");
                     else
-                        if (commandParts.length==1)
-                            throw new IllegalArgumentException("Nu ati specificat fisierul!!");
-                        else
-                            throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
+                        throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
                 }
+                if (commandParts[0].equals("cd")) {
+                    if (commandParts.length == 2) {
+                        con.cd(commandParts[1]);
+                        continue;
+                    } else if (commandParts.length == 1)
+                        throw new IllegalArgumentException("Nu ati specificat directorul!");
+                    else
+                        throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
+                }
+                if (commandParts[0].equals("search")) {
+                    if (commandParts.length == 3) {
+                        Pattern p = Pattern.compile(commandParts[1]);
+                        con.search(p, commandParts[2]);
+                        continue;
+                    } else if (commandParts.length == 2)
+                        throw new IllegalArgumentException("Nu ati specificat fisierul sau directorul!");
+                    else
+                        throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
+                }
+                if (commandParts[0].equals("rename")) {
+                    if (commandParts.length == 3) {
+                        con.rename(commandParts[1], commandParts[2]);
+                        continue;
+                    } else if (commandParts.length == 2)
+                        throw new IllegalArgumentException("Numar de parametrii invalid!");
+                    else
+                        throw new IllegalArgumentException("Pathul nu trebuie sa contina spatii!");
+                }
+                throw new IllegalArgumentException("Comanda invalida!");
             } catch (IllegalArgumentException e) {
                 //e.printStackTrace();
                 System.out.println(e.getMessage());
